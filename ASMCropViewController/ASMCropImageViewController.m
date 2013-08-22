f24ee86b11c19fd6433a0067ad5431db5e149bd1
Andy Molloy <asmolloy@me.com>
2013-08-22 08:59:58 -0400

    Set up the scroll view and image view, allow the user to zoom around the image if it's too big to fit on the screen.

--- ASMCropViewController/ASMCropImageViewController.m
+++ ASMCropViewController/ASMCropImageViewController.m
@@ -8,22 +8,73 @@
 
 #import "ASMCropImageViewController.h"
 
-@interface ASMCropImageViewController ()
-
+@interface ASMCropImageViewController () <UIScrollViewDelegate>
+@property (strong, nonatomic) UIScrollView* scrollView;
+@property (strong, nonatomic) UIImageView* imageView;
 @end
 
 @implementation ASMCropImageViewController
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+	self.scrollView.delegate = self;
+	[self.view addSubview:self.scrollView];
+	
+	self.imageView = [[UIImageView alloc] initWithFrame:self.scrollView.bounds];
+	[self.scrollView addSubview:self.imageView];
+	
+	[self setupImageView];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)setupImageView
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	[self.imageView setImage:self.image];
+	[self.imageView sizeToFit];
+
+	self.scrollView.contentSize = self.imageView.frame.size;
+	
+	CGFloat zoomScale = MIN(CGRectGetWidth(self.scrollView.frame) / self.scrollView.contentSize.width,
+							CGRectGetHeight(self.scrollView.frame) / self.scrollView.contentSize.height);
+	
+	self.scrollView.minimumZoomScale = zoomScale;
+	self.scrollView.zoomScale = zoomScale;
+}
+
+- (void)setImage:(UIImage *)image
+{
+	_image = image;
+	[self setupImageView];
+}
+
+#pragma mark Scroll View Delegate
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+	return self.imageView;
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(float)scale
+{
+	
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+	CGFloat offsetX = 0.0f;
+	if ( scrollView.bounds.size.width > scrollView.contentSize.width )
+	{
+		offsetX = (scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5f;
+	}
+
+	CGFloat offsetY = 0.0f;
+	if ( scrollView.bounds.size.height > scrollView.contentSize.height )
+	{
+		offsetY = (scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5f;
+	}
+
+    self.imageView.center = CGPointMake(scrollView.contentSize.width * 0.5f + offsetX,
+										scrollView.contentSize.height * 0.5f + offsetY);
 }
 
 @end
