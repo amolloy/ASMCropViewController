d5089e4ea3d5f51a1e6a9682410faeb44fdfd63b
Andy Molloy <asmolloy@me.com>
2013-08-25 09:11:56 -0400

    Add ability to show the cropped image in the demo app.

--- ASMCropViewController/ASMAppDelegate.m
+++ ASMCropViewController/ASMAppDelegate.m
@@ -9,46 +9,43 @@
 #import "ASMAppDelegate.h"
 #import "ASMCropImageViewController.h"
 
+@interface ASMAppDelegate ()
+@property (nonatomic, strong) ASMCropImageViewController* cropImageViewController;
+@end
+
 @implementation ASMAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	
-	ASMCropImageViewController* cropImageViewController = [[ASMCropImageViewController alloc] init];
-	cropImageViewController.image = [UIImage imageNamed:@"IMG_7999.jpg"];
-	self.window.rootViewController = cropImageViewController;
+	self.cropImageViewController = [[ASMCropImageViewController alloc] init];
+	self.cropImageViewController.image = [UIImage imageNamed:@"IMG_7999.jpg"];
+
+	UINavigationController* navController = [[UINavigationController alloc]
+											 initWithRootViewController:self.cropImageViewController];
+	
+	navController.topViewController.navigationItem.rightBarButtonItem =
+	[[UIBarButtonItem alloc] initWithTitle:@"Crop"
+									 style:UIBarButtonItemStyleBordered
+									target:self
+									action:@selector(doCrop:)];
+	
+	self.window.rootViewController = navController;
 	
 	[self.window makeKeyAndVisible];
 	
     return YES;
 }
-							
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+- (void)doCrop:(id)sender
 {
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+	UIImage* croppedImage = [self.cropImageViewController croppedImage];
+	UIImageView* croppedImageView = [[UIImageView alloc] initWithImage:croppedImage];
+	UIScrollView* scrollView = [[UIScrollView alloc] initWithFrame:self.window.bounds];
+	[scrollView addSubview:croppedImageView];
+	[scrollView setContentSize:croppedImage.size];
+	[self.window addSubview:scrollView];
 }
 
 @end
