da761080fb1359e697c0bb56a5c765d1f084c23f
Andrew Molloy <amolloy@gmail.com>
2014-03-24 18:34:44 -0400

    Fix compiling with 64-bit SDK

--- ASMCropViewController/ASMCropImageViewController.m
+++ ASMCropViewController/ASMCropImageViewController.m
@@ -100,7 +100,7 @@
 {
 }
 
-- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(float)scale
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale
 {
 }
 
